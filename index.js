abc2b9bd06ea1c5d2018d069f81d87701e0e23a7
Noureddine ABABAR <noureddineababar1@gmail.com>
2021-10-29 15:51:23 +0100

    show

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ app.get('/equipes', (req, res) => {
     // res.send('Liste des equipes')
     res.status(200).json(equipes);
 })
+app.get('/equipes/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const equipe = equipes.find(equipe => equipe.id === id)
+    res.status(200).json(equipe);
+})
